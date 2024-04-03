189ab999ee47f5633829e9d26eff9327883a3db6
masilova23 <144823004+masilova23@users.noreply.github.com>
2024-04-03 19:53:55 +0300

    Update server.php

--- TASK3/server.php
+++ TASK3/server.php
@@ -18,7 +18,7 @@
   $gen = mysqli_real_escape_string($conn, $_POST['gen']);
   $about = mysqli_real_escape_string($conn, $_POST['about']);
 
-  $lengs = $_POST[leng];
+  $lengs = $_POST[filter.input('leng')];
   $arr_len = ["Pascal","C","C++","JavaScript","PHP","Python","Java","Haskel","Clijure","Prolog","Scara"];
   $arr_num_len = [0,0,0,0,0,0,0,0,0,0,0];
 
